5fd0211bd99a96cdac2ed4938baa2da0a848e1d4
HDJ <263463764@qq.com>
2018-11-27 11:14:26 +0800

    添加异常捕获代码

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 export function render(oldRender) {
   if (navigator.userAgent.indexOf('Windows') === -1) {
-    alert(123)
     oldRender();
   }else{
-    alert(456)
     oldRender();
   }
 }
--- src/config.js
+++ src/config.js
@@ -5,3 +5,7 @@ if(!window.MC){
 // window.MC.HOST = 'http://112.74.26.228:10080';
 window.MC.HOST = 'http://120.79.77.207:8080';
 console.log(window.MC);
+window.onerror = function(errorMessage, scriptURI, lineNumber,columnNumber,errorObj) {
+    // TODO
+    alert(JSON.stringify(errorMessage))
+}
